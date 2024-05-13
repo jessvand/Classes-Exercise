b042344c43246c30482bd42d79520e802a84c7a7
jessvand <jmvandbeek@gmail.com>
2024-05-13 14:19:28 -0400

    Classes and Car Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var ShirleyTilda = new Car();
+
+            ShirleyTilda.Make = "Chevrolet";
+            ShirleyTilda.Model = "Cobalt";
+            ShirleyTilda.Year = "2008";
+
+            Console.WriteLine($"My car is a {ShirleyTilda.Year} {ShirleyTilda.Make} {ShirleyTilda.Model} and her name is Shirley Tilda");
+
         }
     }
 }
