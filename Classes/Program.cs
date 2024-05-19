1f0dfd01755e6259137c921c3f42c6d588ac61f7
jessvand <jmvandbeek@gmail.com>
2024-05-19 12:50:21 -0400

    Redid Ex 1 for classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var ShirleyTilda = new Car();
+
+            ShirleyTilda.Make = "Chevrolet";
+            ShirleyTilda.Model = "Cobalt";
+            ShirleyTilda.Year = (2009);
+
+            Console.WriteLine($"My car's a {ShirleyTilda.Year} {ShirleyTilda.Make} {ShirleyTilda.Model}");
+
         }
     }
 }
